072d6e92b5dc44bff60d13d9794a38bf6297b98a
Samuel-David-Angus <angus.samuel.dc@gmail.com>
2022-12-17 15:07:40 +0800

    Complete SimpleCalc

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -3,6 +3,8 @@ package SimpleCalc;
 import LeapYear.LeapYearGUI;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SimpleCalcGUI extends JFrame{
     private JPanel pnlMain;
@@ -12,11 +14,48 @@ public class SimpleCalcGUI extends JFrame{
     private JTextField tfNumber2;
     private JTextField tfResult;
 
+    public SimpleCalcGUI() {
+        super("Simple Calculator");
+
+        btnCompute.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                int num1 = Integer.parseInt(tfNumber1.getText());
+                int num2 = Integer.parseInt(tfNumber2.getText());
+                int result = 0;
+                String operation = cbOperations.getSelectedItem().toString();
+                if (operation == "+") {
+                    result = add(num1, num2);
+                } else if (operation == "-") {
+                    result = subtract(num1, num2);
+                } else if (operation == "*") {
+                    result = multiply(num1, num2);
+                } else if (operation == "/") {
+                    result = divide(num1, num2);
+                }
+                tfResult.setText(Integer.toString(result));
+            }
+        });
+    }
+
     public static void main(String[] args) {
         SimpleCalcGUI app = new SimpleCalcGUI();
         app.setContentPane(app.pnlMain);
-        app.setSize(500, 500);
+        app.setSize(550, 500);
         app.setDefaultCloseOperation(EXIT_ON_CLOSE);
         app.setVisible(true);
+    }
+
+    public int add(int num1, int num2) {
+        return num1 + num2;
+    }
+    public int subtract(int num1, int num2) {
+        return num1 - num2;
+    }
+    public int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+    public int divide(int num1, int num2) {
+        return num1 / num2;
     }
 }
